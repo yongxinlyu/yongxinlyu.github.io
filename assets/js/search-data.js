3a21c280b0ff99d0ce40f1800d251f08c78612ff
yongxinlyu <103477685+yongxinlyu@users.noreply.github.com>
2025-11-10 09:58:27 +0000

    Deploying to gh-pages from @ yongxinlyu/yongxinlyu.github.io@7952a3bc0e1d527adc1bf227014b4c5f11f3d84b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-publications",
           title: "publications",
-          description: "see my google scholar page for a full list of publications.",
+          description: "&#39;see my google scholar page for a full list of publications.&#39;",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
