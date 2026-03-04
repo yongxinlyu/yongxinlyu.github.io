f09ec69c25f7d2cc88e8deeb90e1195fa49921b4
yongxinlyu <103477685+yongxinlyu@users.noreply.github.com>
2026-03-04 00:13:48 +0000

    Deploying to gh-pages from @ yongxinlyu/yongxinlyu.github.io@43088521c7d41f2d26d70cc1befc7b7d0013c413 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,18 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-topic-modelling-learning-journal-part-3",
-        
-          title: "Topic modelling learning journal (part 3)",
-        
-        description: "to the wonderland of natural language processing",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2026/topic-modelling-III/";
-          
-        },
-      },{id: "post-topic-modelling-learning-journal-part-2",
+        },{id: "post-topic-modelling-learning-journal-part-2",
         
           title: "Topic modelling learning journal (part 2)",
         
